a0d013333278724cb3e1df13dcab0fe16a465c0d
luizAzevedo <lfaazevedo@hotmail.com>
2020-06-29 21:21:26 -0300

    Introduçao aos Templates

--- primeiro-projeto/src/app/cursos/cursos-detalhes/cursos-detalhes.component.ts
+++ primeiro-projeto/src/app/cursos/cursos-detalhes/cursos-detalhes.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cursos-detalhes',
   templateUrl: './cursos-detalhes.component.html',
-  styleUrls: ['./cursos-detalhes.component.css']
+  styleUrls: ['./cursos-detalhes.component.css'],
 })
 export class CursosDetalhesComponent implements OnInit {
+  nomePortal: string;
 
-  constructor() { }
+  cursos: string[] = ['Java', 'Ext JS', 'Angular'];
 
-  ngOnInit(): void {
+  constructor() {
+    this.nomePortal = 'http://loiane.training.com';
   }
 
+  ngOnInit(): void {}
 }
